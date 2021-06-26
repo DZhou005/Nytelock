e5b63a73259b16818369f8a2ee18ff81a337607d
DZhou005 <74795454+DZhou005@users.noreply.github.com>
2021-06-26 16:16:48 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+
+
 
 function App() {
+  const [info, setInfo] = useState('lettermark')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='container'>
+     <div className='infoBox'>
+       <div className='twoLinesOfText'>
+        <li className='firstBulletPoint'>HOW DO I KNOW WHICH LOGO STYLE IS RIGHT FOR ME?</li>
+        <h1 className='text1'>Different Types of Logo Styes</h1>
+       </div>
+       <div className='contactUsButtonDiv'>
+        <button onClick={() => window.location.assign('https://nytelock.com/quote/')}className='contactUsButton'>Contact Us {<img className='arrowButton' src={process.env.PUBLIC_URL + '/arrow.png'} alt='none'></img>}</button>
+       </div>
+      <button>Lettermark <br/> Logos</button>
+     </div>
+
+   </div>
   );
 }
 
