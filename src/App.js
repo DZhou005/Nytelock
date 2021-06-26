78bdf183fd1435bf501a5dcbb25262867337c7ea
DZhou005 <74795454+DZhou005@users.noreply.github.com>
2021-06-26 16:58:16 -0700

    adding info for each button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ import React, { useState } from 'react'
 
 function App() {
   const [info, setInfo] = useState('lettermark')
+
+  const infoDisplay = () => {
+    if(info === 'lettermark') {
+      return (
+        <div className='infoDisplay'>
+          <h1 className='infoDisplayTitle'>Lettermark Logos</h1>
+        </div>
+
+      )
+    }
+  }
+
+
+
   return (
    <div className='container'>
      <div className='infoBox'>
@@ -15,10 +29,18 @@ function App() {
        <div className='contactUsButtonDiv'>
         <button onClick={() => window.location.assign('https://nytelock.com/quote/')}className='contactUsButton'>Contact Us {<img className='arrowButton' src={process.env.PUBLIC_URL + '/arrow.png'} alt='none'></img>}</button>
        </div>
-      <button>Lettermark <br/> Logos</button>
+      <div className='infoButtons'>
+        <button onClick={() => setInfo('lettermark')}>Lettermark <br/> Logos</button>
+        <button onClick={() => setInfo('wordmark')}>Wordmark <br /> Logos</button>
+        <button onClick={() => setInfo('pictorial')}>Pictorial Mark <br /> Logos</button>
+        <button onClick={() => setInfo('abstract')}>Abstract Mark <br /> Logos</button>
+        <button onClick={() => setInfo('mascot')}>Mascot <br /> Logos</button>
+        <button onClick={() => setInfo('combination')}>Combination<br /> Logos</button>
+      </div>
      </div>
-
+     {infoDisplay()}
    </div>
+
   );
 }
 
