5b7d33d3679899012d30d5b8d5aa7335ed45639f
DZhou005 <74795454+DZhou005@users.noreply.github.com>
2021-06-26 17:56:09 -0700

    about half way done with loading info when clicked

--- src/App.js
+++ src/App.js
@@ -9,10 +9,50 @@ function App() {
   const infoDisplay = () => {
     if(info === 'lettermark') {
       return (
-        <div className='infoDisplay'>
-          <h1 className='infoDisplayTitle'>Lettermark Logos</h1>
+        <div className='infoContainer'>
+          <div className='infoDisplay'>
+            <h1 className='infoDisplayTitle'>Lettermark Logos</h1>
+            <p className='infoDisplayDescription'>A Monogram or lettermark is a typography-based logo that's comprised of a few letters, usually a company's initials. The lettermark is all about simplicity.<br/><br/>By utilizing just a few letters lettermark logos are effective at streamlining any company brand if they have a long name.</p>
+          </div>
+          <div className='infoPicContainer'>
+            <img className='infoPics' src={process.env.PUBLIC_URL + '/HBO.png'} alt='none'></img>
+            <img className='infoPics' src={process.env.PUBLIC_URL + '/IBM.png'} alt='none'></img>
+            <img className='infoPics' src={process.env.PUBLIC_URL + '/Nasa.png'} alt='none'></img>
+          </div>
         </div>
 
+
+
+      )
+    }
+    else if(info === 'wordmark') {
+      return (
+        <div className='infoContainer'>
+          <div className='infoDisplay'>
+            <h1 className='infoDisplayTitle'>Wordmark Logos</h1>
+            <p className='infoDisplayDescription'>Similar to a lettermark, a wordmark or logotype is a font-based logo that focuses on a business' name alone. Think Visa and Coca-Cola. Wordmark logos work really well when a company has a succinct and distinct name. Google's logo is a great example of this. The name itself is catchy and memorable so, when combined with strong typography, the logo helps create strong brand recognition.</p>
+          </div>
+          <div className='infoPicContainer'>
+            <img className='infoPics' src={process.env.PUBLIC_URL + '/visa.png'} alt='none'></img>
+            <img className='infoPics' src={process.env.PUBLIC_URL + '/coke.png'} alt='none'></img>
+            <img className='infoPics' src={process.env.PUBLIC_URL + '/google.png'} alt='none'></img>
+          </div>
+        </div>
+      )
+    }
+    else if(info === 'pictorial') {
+      return (
+        <div className='infoContainer'>
+          <div className='infoDisplay'>
+            <h1 className='infoDisplayTitle'>Pictorial Mark Logos</h1>
+            <p className='infoDisplayDescription'>A pictorial mark (sometimes called brand mark or logo symbol) is an icon--or graphic-based logo. It's probably the image that comes to mind when you think "logo":the iconic Apple logo, the Twitter bird, the Target bullseye. Each of these companies' logos is so emblematic, and each brand so established, that the mark alone is instantly recognizable.</p>
+          </div>
+          <div className='infoPicContainer'>
+            <img className='infoPics' src={process.env.PUBLIC_URL + '/visa.png'} alt='none'></img>
+            <img className='infoPics' src={process.env.PUBLIC_URL + '/coke.png'} alt='none'></img>
+            <img className='infoPics' src={process.env.PUBLIC_URL + '/google.png'} alt='none'></img>
+          </div>
+        </div>
       )
     }
   }
